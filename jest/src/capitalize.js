caaf0287f88fac5cd12dfa2513b39947f84d4c1a
andrecio <andreciobezerra@gmail.com>
2021-01-05 18:43:50 -0300

    cleaning

--- jest/src/capitalize.js
+++
@@ -1,8 +0,0 @@
-export const capitalize = (str) => {
-  if (typeof str !== 'string') throw Error('Invalid input!')
-
-  const lowString = str.toLowerCase()
-
-  return `${lowString.charAt(0).toUpperCase()}${lowString.slice(1)}`
-}
-
--- jest/src/reverseString.js
+++
@@ -1,5 +0,0 @@
-export const reverseString = (str) =>{
-  if(typeof str !== 'string') throw Error('Invalid input!')
-
-  return [...str].reverse().join('')
-}
